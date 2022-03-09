81d17b3ff58ce6f28ab3fe83297d157c02e1f82a
lights93 <lights8615@gmail.com>
2022-03-09 21:19:53 +0900

    refactor: PATTERN 적용

--- src/main/java/minhoyoo/calculator/view/Input.java
+++ src/main/java/minhoyoo/calculator/view/Input.java
@@ -1,14 +1,17 @@
 package minhoyoo.calculator.view;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Input {
 	private static final Scanner scanner = new Scanner(System.in);
+	private static final Pattern REMOVE_ALL_SPACES_PATTERN = Pattern.compile("\\s+");
+	private static final String EMPTY = "";
 
 	private Input() {
 	}
 
 	public static String getPoints() {
-		return scanner.nextLine().replaceAll("\\s+", "");
+		return REMOVE_ALL_SPACES_PATTERN.matcher(scanner.nextLine()).replaceAll(EMPTY);
 	}
 }
