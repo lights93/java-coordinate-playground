151c973dd2b63ac358d222e6662584cbd7eb1c26
lights93 <lights8615@gmail.com>
2022-03-09 15:08:30 +0900

    feat: X, Y좌표 모두 최대 24까지만 입력 가능

--- src/main/java/minhoyoo/calculator/domain/Coordinate.java
+++ src/main/java/minhoyoo/calculator/domain/Coordinate.java
@@ -1,16 +1,26 @@
 package minhoyoo.calculator.domain;
 
 public class Coordinate {
+	private static final int MIN_VALUE = 1;
+	private static final int MAX_VALUE = 24;
 	private final int x;
 	private final int y;
 
 	private Coordinate(int x, int y) {
+		checkValue(x);
+		checkValue(y);
 		this.x = x;
 		this.y = y;
 	}
 
 	public static Coordinate create(int x, int y) {
 		return new Coordinate(x, y);
+	}
+
+	private void checkValue(int val) {
+		if (val < MIN_VALUE || val > MAX_VALUE) {
+			throw new IllegalArgumentException("잘못된 좌표값입니다.");
+		}
 	}
 
 	public int getX() {
--- src/test/java/minhoyoo/calculator/domain/CoordinateTest.java
+++ src/test/java/minhoyoo/calculator/domain/CoordinateTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CoordinateTest {
 	@DisplayName("좌표 제대로 생성되는지 확인")
@@ -13,5 +15,14 @@ class CoordinateTest {
 
 		assertThat(coordinate.getX()).isEqualTo(1);
 		assertThat(coordinate.getY()).isEqualTo(2);
+	}
+
+	@DisplayName("생성 시 에러")
+	@ParameterizedTest
+	@CsvSource(value = {"1,0", "0,1", "25,1", "1,25"})
+	void contructError(int x, int y) {
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> Coordinate.create(x, y))
+			.withMessage("잘못된 좌표값입니다.");
 	}
 }
